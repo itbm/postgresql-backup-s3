812f4dc6c8e9ff1f6ea77c717f92ceb97edf110f
Ali <alilesani77549@gmail.com>
2026-06-04 07:22:59 +0330

    fix if block  typo

--- backup.sh
+++ backup.sh
@@ -49,9 +49,10 @@ if [ "${S3_ENDPOINT}" == "**None**" ]; then
   AWS_ARGS=""
 else
   if [ "${S3_SSL_VERIFY}" == "false" ]; then
-    AWS_ARGS="--endpoint-url --no-verify-ssl ${S3_ENDPOINT}"
+    AWS_ARGS="--endpoint-url ${S3_ENDPOINT} --no-verify-ssl"
   else
     AWS_ARGS="--endpoint-url ${S3_ENDPOINT}"
+  fi
 fi
 
 export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
